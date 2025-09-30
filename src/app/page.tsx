7f220ee31a8a7d0282339aebe8afb0602bdf28e7
Ranko <31764711+braj83@users.noreply.github.com>
2025-09-30 16:24:45 +0200

    added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import RandomQuoteHeader from "@/components/hero/hero";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <div data-theme="dark">
       <main className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen">
         <RandomQuoteHeader />
+        <Analytics />
       </main>
     </div>
   );
